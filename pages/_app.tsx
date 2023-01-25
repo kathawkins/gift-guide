b3bb0af7f74f9cf94ca4d2e1f0b2a61b8ea6735b
kathawkins <kmoreno1205@gmail.com>
2023-01-24 17:37:15 -0700

    Repo, Database, and User Auth setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '@/styles/globals.css'
+// The setup below can be used to determine if a user is authenticated by checking 
+// that the user object returned by the useUser() hook is defined
+// https://supabase.com/docs/guides/auth/auth-helpers/nextjs
+
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider, Session } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({
+  Component,
+  pageProps,
+}: AppProps<{
+  initialSession: Session
+}>) {
+  // Create a new supabase browser client on every first render.
+  const [supabase] = useState(() => createBrowserSupabaseClient())
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
 }
+export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,12 +2,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html className="bg-white" lang="en">
+    {/* <div> */}
       <Head />
       <body>
         <Main />
         <NextScript />
       </body>
+    {/* </div> */}
     </Html>
   )
 }
